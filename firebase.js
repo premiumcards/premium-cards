8bc8ad4ff85f40b32eaf071c8f9e1ff229be3c86
premiumcards <rayomensukuna0001@gmail.com>
2026-07-15 18:59:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,27 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
 
-// Replace with your own Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyDdsl1ixAlpDTjMtpQCvN8yHlj2sHmJesY",
+  authDomain: "premium-cards-380c2.firebaseapp.com",
+  projectId: "premium-cards-380c2",
+  storageBucket: "premium-cards-380c2.firebasestorage.app",
+  messagingSenderId: "366925861411",
+  appId: "1:366925861411:web:bcb88639698abe8fe8d796",
+  measurementId: "G-GPPKYETTHC"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 };
 
 const app = initializeApp(firebaseConfig);
